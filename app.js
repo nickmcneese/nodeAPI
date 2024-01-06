3ec39087d2e28d4a30fc33b2ea3a758eb9b69a1b
Nick McNeese <nickmcneese@berkeley.edu>
2024-01-06 00:25:41 -0800

    more api functionallity with mongoDB

--- app.js
+++ app.js
@@ -2,9 +2,13 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const productRoutes = require("./api/routes/products");
 const orderRoutes = require('./api/routes/orders');
+
+//setting up DB with mongoose
+mongoose.connect('mongodb+srv://nmcneese:' + process.env.MONGO_ATLAS_PW + '@node-rest-testing.bfay2rk.mongodb.net/?retryWrites=true&w=majority')
 
 //logger middleware
 app.use(morgan('dev')); 
@@ -12,10 +16,22 @@ app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
+//allows for CORS control
+app.use((res, req, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', '*');
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).json({});
+    }
+    next();
+});
+
 //Routes which should handle requests
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
 
+//error handling
 app.use((req, res, next) => {
     const error = new Error("Not Found");
     error.status = 404;
